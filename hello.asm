f0e286b5c9363a7e441c02569782782bc76bbf71
Luiz Damas <luizdamasamorim@gmail.com>
2024-10-18 14:37:07 -0300

    primeiro arquivo em nasm. programa hello world

---
+++ hello.asm
@@ -0,0 +1,16 @@
+global _start
+
+    section .text
+    _start:
+        mov rax,1                       ;Prepara o sistema para fazer a escrita de um texto
+        mov rdi,1                       ;Preparar a saida do texto na tela
+        mov rsi,mensagem                ;Imprimir ou exibir a mensagem na tela
+        mov rdx,13                      ;Quantidade de caracteres
+        syscall                         ;Chama o sistema para preparar a saida
+        mov rax,60                      ;Chamada para a saida do sistema
+        xor rdi,rdi                     ;Executar a saida do sistema
+        syscall                         ;Invocar o sistema operacional para fechar
+
+
+        section .data
+        mensagem: db 'Hello,word' ,10   ;O valor 10 representa a quebra de linha  
